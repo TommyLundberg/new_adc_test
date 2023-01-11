26e959b6a3dfa4e039c081c15f9f1e30cc622eae
TommyLundberg <tomlundb@kth.se>
2023-01-11 01:38:06 +0100

    Okej nytt försök

--- src/main.c
+++ src/main.c
@@ -4,9 +4,17 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+
+
 #include <zephyr/zephyr.h>
+#include <drivers/adc.h>
+
+
 
 void main(void)
 {
 	printk("Hello World! %s\n", CONFIG_BOARD);
+	static const struct adc_channel_cfg ch0_cfg_dt = ADC_CHANNEL_CFG_DT(DT_CHILD(DT_NODELABEL(adc), channel_0));
+	static const struct adc_channel_cfg ch1_cfg_dt = ADC_CHANNEL_CFG_DT(DT_CHILD(DT_NODELABEL(adc), channel_1));
+
 }
