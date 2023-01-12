20cc6dd15681144541ca3ee953b317e46abf4583
TommyLundberg <tomlundb@kth.se>
2023-01-12 01:10:12 +0100

    Fixed minor details and added mV readout

--- src/main.c
+++ src/main.c
@@ -10,15 +10,20 @@
 #include <stdio.h>
 #include <zephyr/sys/printk.h>
 #include <zephyr/device.h>
+#include <math.h>
 #include <zephyr/drivers/adc.h>
 
 #define ADC_RESOLUTION 14
 #define ADC_CHANNEL_0  0
 #define ADC_CH0_OVERSAMPLE 8
 #define BUFFER_SIZE 8
-static int16_t sample_buffer_0[BUFFER_SIZE];
+static int16_t sample_buffer_0;
 int err;
 bool state;
+int32_t real_mv;
+int32_t gain_inv = 5;
+int32_t ref_mv = 600;
+
 
 /* adc read sequence struct with options to be fed into 
 the adc_read() function */
@@ -26,12 +31,12 @@ const struct adc_sequence_options ch0_sequence_opts = {
 	.interval_us = 0,
 	.callback = NULL,
 	.user_data = NULL,
-	.extra_samplings = 7,
+	.extra_samplings = 0,
 	};
 struct adc_sequence adc_ch0_seq = {
 		.options = &ch0_sequence_opts,
 		.channels = BIT(ADC_CHANNEL_0),
-		.buffer = sample_buffer_0,
+		.buffer = &sample_buffer_0,
 		.buffer_size = sizeof(sample_buffer_0),
 		.resolution = ADC_RESOLUTION,
 		.oversampling = ADC_CH0_OVERSAMPLE,
@@ -48,7 +53,8 @@ void main(void){
 	
 	err = adc_channel_setup(dev_adc, &ch0_cfg_dt);
 		if (err!=0){
-			printk("ADC channel setup failed, error %s\n",err);
+			printk("ADC channel setup failed, error %d\n",err);
+			return -1;
 		}
 		else{
 			printk("ADC channel setup successfull\n");
@@ -57,17 +63,20 @@ void main(void){
 	state = device_is_ready(dev_adc);
 		if (state==false){
 			printk("ADC Channel not ready");
-		return -1;
+			return -1;
 		}
 	
 	while (1){
 
+		
 		err = adc_read(dev_adc, &adc_ch0_seq);
 			if (!err){
 				printk("ADC read successfull\n");
-				for(int i=0;i<BUFFER_SIZE;i++){
-					printk("Raw value read: %d\n",sample_buffer_0[i]);
-				}
+				k_sleep(K_MSEC(10));
+				printk("Raw value read: %d\n",sample_buffer_0);
+				real_mv = (ref_mv*gain_inv*sample_buffer_0)>>(ADC_RESOLUTION-1);
+				printk("mV: %d\n", real_mv);
+				
 			}
 			else{
 				printk("NO READOUT FOR YOU!!!\n");
